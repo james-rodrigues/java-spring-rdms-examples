23334d698ddf4bec68f9634e85a1c298ecbe71ff
james-rodrigues <james.992rodrigues@gmail.com>
2022-03-05 20:52:54 +0530

    Initial Setup

---
+++ demo-example-r2dbc/src/main/java/net/database/example/r2dbc/DemoExampleR2dbcApplication.java
@@ -0,0 +1,13 @@
+package net.database.example.r2dbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoExampleR2dbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoExampleR2dbcApplication.class, args);
+	}
+
+}
---
+++ demo-example-r2dbc/src/test/java/net/database/example/r2dbc/DemoExampleR2dbcApplicationTests.java
@@ -0,0 +1,13 @@
+package net.database.example.r2dbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoExampleR2dbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
